bed4f2cf657d81de9ee1fe8061ae021b7123794c
Kamil Tomczyk <kamil.t@moodup.team>
2024-09-16 22:30:30 +0200

    Remove unused imports

--- app/src/main/java/kamiltomczyk/recruitment/exchangeratesapp/data/repositories/CurrencyRepositoryInterface.kt
+++ app/src/main/java/kamiltomczyk/recruitment/exchangeratesapp/data/repositories/CurrencyRepositoryInterface.kt
@@ -3,7 +3,6 @@ package kamiltomczyk.recruitment.exchangeratesapp.data.repositories
 import kamiltomczyk.recruitment.exchangeratesapp.data.enums.TableName
 import kamiltomczyk.recruitment.exchangeratesapp.data.models.CurrencyRate
 import kamiltomczyk.recruitment.exchangeratesapp.data.models.CurrencyRateHistory
-import kamiltomczyk.recruitment.exchangeratesapp.data.models.Rate
 
 interface CurrencyRepositoryInterface {
     suspend fun getCurrentExchangeRates(): List<CurrencyRate>
