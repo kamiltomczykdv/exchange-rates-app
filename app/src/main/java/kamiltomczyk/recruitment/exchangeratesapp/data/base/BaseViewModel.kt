c57913f66e476b4d131413b4e6dada06998b674a
Kamil Tomczyk <kamil.t@moodup.team>
2024-09-16 21:48:55 +0200

    Prepare base view model

---
+++ app/src/main/java/kamiltomczyk/recruitment/exchangeratesapp/data/base/BaseViewModel.kt
@@ -0,0 +1,26 @@
+package kamiltomczyk.recruitment.exchangeratesapp.data.base
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kamiltomczyk.recruitment.exchangeratesapp.data.states.UIState
+import kotlinx.coroutines.CoroutineExceptionHandler
+import retrofit2.HttpException
+
+abstract class BaseViewModel : ViewModel() {
+    protected val protectedUIState: MutableState<UIState> = mutableStateOf(UIState.Initialized)
+    val uiState: State<UIState> = protectedUIState
+
+    val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        protectedUIState.value = if (exception is HttpException) {
+            UIState.Error(
+                code = exception.code(),
+                message = exception.message
+            )
+        } else {
+            UIState.Error(message = exception.message)
+        }
+        exception.printStackTrace()
+    }
+}
--- app/src/main/java/kamiltomczyk/recruitment/exchangeratesapp/data/states/UIState.kt
+++ app/src/main/java/kamiltomczyk/recruitment/exchangeratesapp/data/states/UIState.kt
@@ -5,7 +5,8 @@ sealed class UIState {
     data object InProgress : UIState()
 
     data class Error(
-        val code: Int?, val message: String?
+        val code: Int? = null,
+        val message: String?
     ) : UIState()
 
     data class NavigateTo(val key: String) : UIState()
